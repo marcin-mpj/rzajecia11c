4d1952fff520728251da23f857a073642da25284
marcin-mpj <marcinm1@yahoo.pl>
2021-04-03 15:54:29 +0200

    add R projekt and downloada data from weather api

---
+++ 1_cwiczenia.R
@@ -0,0 +1,11 @@
+install.packages("httr")
+install.packages("jsonlite")
+
+library(httr)
+require(jsonlite)
+
+endpoint <- "https://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=1765994b51ed366c506d5dc0d0b07b77"
+getWeather <- GET(endpoint)
+weatherText <- content(getWeather, as="text")
+weatherJson <- fromJSON(weatherText)
+weatherDF <- as.data.frame(weatherJson)
